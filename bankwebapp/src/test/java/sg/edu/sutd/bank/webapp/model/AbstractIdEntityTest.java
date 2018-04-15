ce14872f756b4fcc9b5a8f12094d5cd270379994
darrenascione <darren_ng@mymail.sutd.edu.sg>
2018-04-16 00:56:37 +0800

    added PROPER way of testing setters and getters

---
+++ bankwebapp/src/test/java/sg/edu/sutd/bank/webapp/model/AbstractIdEntityTest.java
@@ -0,0 +1,37 @@
+
+import org.junit.Before;
+import org.junit.Test;
+import sg.edu.sutd.bank.webapp.model.AbstractIdEntity;
+import java.lang.reflect.Field;
+
+import static org.junit.Assert.*;
+
+public class AbstractIdEntityTest {
+
+    final AbstractIdEntity abstractIdEntity = new AbstractIdEntity();
+
+
+    @Test
+    public void getId() throws NoSuchFieldException, IllegalAccessException {
+        final Field field = abstractIdEntity.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(abstractIdEntity, 12);
+        final int result = abstractIdEntity.getId();
+        assertEquals("field wasn't retrieved properly", result, 12);
+
+    }
+
+    @Test
+    public void setId() throws NoSuchFieldException, IllegalAccessException {
+        //when
+        abstractIdEntity.setId(12);
+
+        //then
+        final Field field = abstractIdEntity.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(abstractIdEntity), 12);
+
+
+
+    }
+}
