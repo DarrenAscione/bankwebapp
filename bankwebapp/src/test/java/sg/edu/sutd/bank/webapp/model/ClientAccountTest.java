874d7ca9d4b2d24ec9d1a0963bc79885d7191c43
darrenascione <darren_ng@mymail.sutd.edu.sg>
2018-04-16 19:28:01 +0800

    added test cases for clientAccount Model

---
+++ bankwebapp/src/test/java/sg/edu/sutd/bank/webapp/model/ClientAccountTest.java
@@ -0,0 +1,55 @@
+
+import org.junit.Test;
+import sg.edu.sutd.bank.webapp.model.ClientAccount;
+import sg.edu.sutd.bank.webapp.model.User;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+
+import static org.junit.Assert.*;
+
+public class ClientAccountTest {
+
+    final ClientAccount clientAccount = new ClientAccount();
+
+    @Test
+    public void getUser() throws NoSuchFieldException, IllegalAccessException {
+        final Field field = clientAccount.getClass().getDeclaredField("user");
+        field.setAccessible(true);
+        field.set(clientAccount, new User(12));
+        final User result = clientAccount.getUser();
+        final int id = result.getId();
+        assertEquals("field wasn't retrieved properly", id, 12);
+
+    }
+
+    @Test
+    public void setUser() throws NoSuchFieldException, IllegalAccessException {
+        //when
+        clientAccount.setUser(new User(12));
+
+        //then
+        final Field field = clientAccount.getClass().getDeclaredField("user");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(clientAccount), 12);
+    }
+
+    @Test
+    public void getAmount() throws NoSuchFieldException, IllegalAccessException {
+        final Field field = clientAccount.getClass().getDeclaredField("amount");
+        field.setAccessible(true);
+        field.set(clientAccount, new BigDecimal(24));
+        final BigDecimal result = clientAccount.getAmount();
+        assertEquals("field wasn't retrieved properly", result, new BigDecimal(24));
+    }
+
+    @Test
+    public void setAmount() throws NoSuchFieldException, IllegalAccessException {
+        //when
+        clientAccount.setAmount(new BigDecimal(24));
+
+        //then
+        final Field field = clientAccount.getClass().getDeclaredField("amount");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(clientAccount), new BigDecimal(24));
+    }
+}
